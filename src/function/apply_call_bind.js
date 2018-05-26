8a865eec306f3ae32ddd77922eba5c71ba3810c8
gongxia.chen <chengongxia1990@gmail.com>
2018-05-26 16:36:04 -0400

    add call apply and bind example

---
+++ src/function/apply_call_bind.js
@@ -0,0 +1,21 @@
+const person = {
+  printInfo: function (city, state) {
+    return `${this.title} ${this.name} ${city} ${state}`;
+  },
+};
+
+const personArrow = {
+  printInfo: (city, state) => `${this.title} ${this.name} ${city} ${state}`,
+};
+
+const person1 = {
+  title: 'Mr',
+  name: 'superman',
+};
+
+console.log(person.printInfo.apply(person1, ['Jax', 'FL']));
+console.log(person.printInfo.call(person1, 'Jax', 'FL'));
+
+// using arrow function will automatically bind "this" to the scope of declaration
+console.log(personArrow.printInfo.apply(person1, ['Jax', 'FL']));
+console.log(personArrow.printInfo.call(person1, 'Jax', 'FL'));
