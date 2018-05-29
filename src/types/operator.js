1eb15b00c8779f2de731a2d21ab6591a55b180a9
gongxia.chen <chengongxia1990@gmail.com>
2018-05-28 23:39:34 -0400

    add scope chaining and operator example

---
+++ src/types/operator.js
@@ -0,0 +1,11 @@
+var a = 3 + 4 * 5; // 23
+console.log(a);
+
+var a = 2, b = 3, c = 4;
+a = b = c;
+console.log(a);
+console.log(b);
+console.log(c);
+
+console.log(1 < 2 < 3); // true
+console.log(3 < 2 < 1); // true because false < 1
